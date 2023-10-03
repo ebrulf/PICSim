def432f876a93804e3c96364605cec4de79e2264
ebrulf <85363333+ebrulf@users.noreply.github.com>
2023-10-03 12:19:07 +0200

    Dodaj pliki projektów.

---
+++ PICSim/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PICSim
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
